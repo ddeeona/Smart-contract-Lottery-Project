6c33270d7216d467b1e200ec1ce50ed5e74ea442
ddeeona <147994845+ddeeona@users.noreply.github.com>
2023-11-14 18:26:28 +0600

    LinkToken.sol

--- LinkToken.sol
+++ LinkToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.11;
 
 import "@chainlink/contracts/src/v0.4/ERC677Token.sol";
-import {StandardToken as linkStandardToken} from "@chainlink/contracts/src/v0.4/vendor/StandardToken.sol";
+import {StandardToken as linkStandardToken} from "@chainlink/contracts/src/v0.8/vendor/StandardToken.sol";
 
 contract LinkToken is linkStandardToken, ERC677Token {
     uint256 public constant totalSupply = 10**27;
